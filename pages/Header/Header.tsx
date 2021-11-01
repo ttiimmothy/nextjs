f10c797b30ba6d4e33a692ee16714d15af4faf18
timothy <dinosauli2006@gmail.com>
2021-11-01 08:58:57 +0800

    from 29/10-31/10 work

--- pages/Header/Header.tsx
+++ pages/Header/Header.tsx
@@ -29,7 +29,7 @@ export function Header(){
 		<header className={style.header}>
 			<div className={`${style.bar} ${scrollHeight > 10 ? style.bar_scroll : ""}`}>
 				<div className={style.navbar}>
-					<div className={style.major_navbar_part}>
+					<div className={style.left_major_navbar_part}>
             <Link href="/">
               <a className={`${style.navbar_logo} ${scrollHeight > 10 ? style.navbar_small_logo : ""}`}>
                 <div className={style.header_icon}>
@@ -55,19 +55,38 @@ export function Header(){
 							</ul>
 						</nav>
 					</div>
-					<nav className={style.minor_list}>
+					<nav className={style.right_listing}>
 						{scrollHeight > 10 && <div className={style.date_scroll}>2021-09-08</div>}
-						<FontAwesomeIcon icon={["fab","codepen"]} className={style.minor_list_icon} height={18} width={18}/>
-						<FontAwesomeIcon icon="search" className={style.minor_list_icon} height={18} width={18}/>
+						<FontAwesomeIcon icon={["fab","codepen"]} className={style.right_listing_icon} height={18} width={18}/>
+						<FontAwesomeIcon icon="search" className={style.right_listing_icon} height={18} width={18}/>
 						<div className={style.login_button}>登入</div>
 					</nav>
 				</div>
 			</div>
 			<div className={`${style.bar_second_line} ${scrollHeight > 10 ? style.bar_second_line_scroll : ""}`}>
-				<div className={style.category_bar}>
+				<div className={style.weather_category_bar}>
 					<div className={style.weather}>2021-09-08 33℃ 58%</div>
 					<Image src={weather} alt="icon" height={35} width={35}/>
 				</div>
+        <div className={style.category_bar_horizontal_scroll}>
+          <nav className={style.category_bar}>
+            <ul className={`${style.category_items} ${scrollHeight > 10 ? style.category_items_scroll : ""}`}>
+              {
+                categories.map((category,index) =>
+                  (
+                    <li className={style.category_item} key={index}>
+                      <Link href={category.name_en.toLowerCase().split(" ").join("")}>
+                        <a>
+                          {category.name_cn}
+                        </a>
+                      </Link>
+                    </li>
+                  )
+                )
+              }
+            </ul>
+          </nav>
+        </div>
 			</div>
 		</header>
 	)
