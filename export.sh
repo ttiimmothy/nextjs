b39a5bdeea2380f2a12159b98bcb8b18c7cc8deb
timothy <dinosauli2006@gmail.com>
2021-12-01 15:38:56 +0800

    renewal

--- export.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd ../cable-website-next-js-out
-rm -rf _next category channel image tags video 404.html index.html swiper_favicon.ico
-cd ../cable_website_nextjs
-yarn build
-cp -r build ../cable-website-next-js-out
